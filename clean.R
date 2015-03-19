be31b938ac23bff5aebe56788bcf5d2f86dcab13
Polyphant1 <chrisl1991@hotmail.co.uk>
2015-03-19 20:59:42 +0000

    added extra data. moved data processing to clean.R script

--- clean.R
+++ clean.R
@@ -1,20 +1,73 @@
-# Load data once
-airports13<-read.csv("data//Table_12_2_Dom_Air_Pax_Route_Analysis_2013.csv")
+files<-list.files(path = "data\\input",pattern=".csv",full.names = T)
 
-# filter columns
-airports13.filter<-airports13[,c("apt1_apt_name","apt2_apt_name","total_pax_tp","grp_name")]
+nodes<-""
+# get all airport names for manual country assignment
+nodes<-lapply(files,function(x){
+  airports<-read.csv(x)
+  
+  # filter columns
+  airports.filter<-airports[,c("apt1_apt_name","apt2_apt_name")]
+  
+  # generate unique node list with ids
+  c(name=unique(c(as.character(airports.filter[,1]),as.character(airports.filter[,2]))))
+})
 
-# generate unique node list with ids
-nodes<-data.frame(name=unique(c(as.character(airports13.filter[,1]),as.character(airports13.filter[,2]))))
-#nodes<-data.frame(id=seq(from = 1,to=length(nodes),by=1),nodes)
+# filter by unique names, and save to file for manual country assignment in excel
+all_airports<-unique(do.call(c,nodes))
+#write.table(all_airports,file = "data//output//airports_names.csv",sep = ",")
 
-# assign node ids to edges
-edges<-airports13.filter
-edges$apt1_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[1]))
-edges$apt2_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[2]))
+# get manually assigned groups
+airport_groups<-read.csv("data//output//airports.csv",sep=",",header=T)
 
-edges$apt1_id<-edges$apt1_id-1
-edges$apt2_id<-edges$apt2_id-1
+lapply(files,function(x){
+  # Load data
+  airports<-read.csv(x)
+  
+  # filter columns
+  airports.filter<-airports[,c("apt1_apt_name","apt2_apt_name","total_pax_tp","grp_name")]
+  
+  # generate unique node list with ids
+  nodes<-data.frame(name=unique(c(as.character(airports.filter[,1]),as.character(airports.filter[,2]))))
+  
+  edges<-airports.filter
+  edges$apt1_id<-apply(airports.filter,1,function(x) which(nodes[,c("name")] %in% x[1]))
+  edges$apt2_id<-apply(airports.filter,1,function(x) which(nodes[,c("name")] %in% x[2]))
+  
+  # zero-index the edge id's
+  edges$apt1_id<-edges$apt1_id-1
+  edges$apt2_id<-edges$apt2_id-1
+  
+  # normalise passenger number counts to prevent excessively wide edges
+  edges$total_pax_tp<-edges$total_pax_tp/10000
+  
+  # regex year from filename
+  ind<-regexpr("(\\d{4})",files[1])[1]
+  year<-substr(files[1],ind,ind+3)
+  
+  write.table(nodes,paste("data//output//",year,"_nodes.csv"),sep=",")
+  write.table(edges,paste("data//output//",year,"_edges.csv"),sep=",")
+})
 
-edges$total_pax_tp<-edges$total_pax_tp/10000
-
+# Load data
+# airports<-read.csv("data//input//Table_12_2_Dom_Air_Pax_Route_Analysis_2013.csv")
+# airports13<-read.csv("data//input//Table_12_2_Dom_Air_Pax_Route_Analysis_2013.csv")
+# 
+# # filter columns
+# airports13.filter<-airports13[,c("apt1_apt_name","apt2_apt_name","total_pax_tp","grp_name")]
+# 
+# # generate unique node list with ids
+# nodes13<-data.frame(name=unique(c(as.character(airports13.filter[,1]),as.character(airports13.filter[,2]))))
+# #nodes<-data.frame(id=seq(from = 1,to=length(nodes),by=1),nodes)
+# 
+# # assign node ids to edges
+# edges<-airports13.filter
+# edges$apt1_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[1]))
+# edges$apt2_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[2]))
+# 
+# edges$apt1_id<-edges$apt1_id-1
+# edges$apt2_id<-edges$apt2_id-1
+# 
+# edges$total_pax_tp<-edges$total_pax_tp/10000
+# 
+# write.table(nodes,"data//output//13_nodes.csv",sep=",")
+# write.table(edges,"data//output//13_edges.csv",sep=",")
--- server.R
+++ server.R
@@ -1,25 +1,8 @@
 # Load packages
 library(d3Network)
 
-# Load data once
-airports13<-read.csv("data//Table_12_2_Dom_Air_Pax_Route_Analysis_2013.csv")
-
-# filter columns
-airports13.filter<-airports13[,c("apt1_apt_name","apt2_apt_name","total_pax_tp","grp_name")]
-
-# generate unique node list with ids
-nodes<-data.frame(name=unique(c(as.character(airports13.filter[,1]),as.character(airports13.filter[,2]))))
-#nodes<-data.frame(id=seq(from = 1,to=length(nodes),by=1),nodes)
-
-# assign node ids to edges
-edges<-airports13.filter
-edges$apt1_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[1]))
-edges$apt2_id<-apply(airports13.filter,1,function(x) which(nodes[,c("name")] %in% x[2]))
-
-edges$apt1_id<-edges$apt1_id-1
-edges$apt2_id<-edges$apt2_id-1
-
-edges$total_pax_tp<-edges$total_pax_tp/10000
+nodes<-read.csv("data//output//13_nodes.csv",sep=",")
+edges<-read.csv("data//output//13_edges.csv",sep = ",")
 
 #### Shiny ####
 shinyServer(function(input, output) {
